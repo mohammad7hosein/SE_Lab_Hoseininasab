96a813d9863e0a21168976200bb12d9fb3d90bff
mohammad7hosein <mohamad77hosein77@gmail.com>
2022-04-08 02:08:59 +0430

    Initial commit

---
+++ src/Sezar.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Sezar {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        String sez = s.next();
+        int[] m = new int[sez.length()];
+        for(int i = 0; i < sez.length(); i++){
+            if((((int)sez.charAt(i) <= 90) && (((int)sez.charAt(i) + (n % 26)) > 90)) || (((int)sez.charAt(i) >= 97) && ((int)sez.charAt(i) <= 122) && (((int)sez.charAt(i) + (n % 26)) > 122)))
+                m[i] = ((int)sez.charAt(i) + (n % 26) - 26);
+            else
+                m[i] = ((int)sez.charAt(i) + (n % 26));
+        }
+        for(int i = 0; i < sez.length(); i++){
+            System.out.print((char)m[i]);
+        }
+    }
+}
